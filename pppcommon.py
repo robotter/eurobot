6bdeec9847b4781de80605653335f9cf01e326e6
Benoît Ryder <benoit@ryder.fr>
2013-04-27 19:43:57 +0200

    Add a Python script with common stuff to play with PPP
    This script is mostly for convenience, to not bother importing all the
    usual things from here and there.
    
    'import *' it from another script, or %run it from IPython.
    
    Imports Frame, Connection, system payloads, ...
    Access ROOM payloads as room.message_name.
    Access Rob'Otter PPP addresses as addrs.node_name.

---
+++ pppcommon.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+# This file define useful stuff to play with PPP.
+# Import it from another script:
+#   from pppcommon import *
+# Run it from a IPython shell:
+#   %run pppcommon.py
+
+import perlimpinpin
+from perlimpinpin import payload
+from perlimpinpin.frame import Frame
+from perlimpinpin.io import Connection, HubBase, HubClient
+from perlimpinpin.payload.system import *
+from perlimpinpin.payload.log import *
+from perlimpinpin.payload.room import room_payloads
+import time  # for time()/sleep()
+
+import os
+import imp
+
+# load transactions
+imp.load_source('room_transactions', os.path.join(os.path.dirname(__file__), 'room_transactions.py'))
+
+# Gather ROOM payloads, accessed by message name
+class room:
+  for cls in room_payloads.values():
+    locals()[cls.mname] = cls
+
+# Known Rob'Otter PPP addresses
+class addrs:
+  hub = 0x10
+  prop = 0x11
+  meca = 0x12
+  r3d2 = 0x13
+  pmi = 0x14
+
+
