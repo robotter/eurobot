82f6e3f41ef2af211b306e9993172f84a4229356
lamygalle@gmail.com <lamygalle@gmail.com>
2014-05-06 21:19:53 +0200

    corrected adc module

--- projects/color_proximity_sensor/adc.c
+++ projects/color_proximity_sensor/adc.c
@@ -3,13 +3,32 @@
 
 uint16_t adc_GetValue(ADC_t *Adc, uint8_t channel)
 {
+  Adc-> CTRLA = ADC_ENABLE_bm; 
   Adc->REFCTRL = ADC_REFSEL_INT1V_gc | ADC_BANDGAP_bm;
+  Adc->REFCTRL = ADC_REFSEL_VCC_gc;
   Adc->CH0.MUXCTRL = (channel << ADC_CH_MUXPOS0_bp) & ADC_CH_MUXPOS_gm;
-
-  /* ctart conversion in single ended */
-  Adc->CH0.CTRL = ADC_CH_START_bm | ADC_CH_INPUTMODE0_bm;
+  /* start conversion in single ended */
+  Adc->CH0.CTRL = ADC_CH_START_bm | ADC_CH_INPUTMODE_SINGLEENDED_gc;
+  
   /* wait for end of conversion */
   while((Adc->INTFLAGS & ADC_CH0IF_bm) == 0x00u); 
 
-  return Adc->CH0RES;
+  // adc incorporates an offset of 205 to ensure proper 0V measure)
+  return (Adc->CH0RES - 515u);
+}
+
+uint16_t adc_Get3v3Value(ADC_t *Adc)
+{
+  Adc-> CTRLA = ADC_ENABLE_bm; 
+  Adc->REFCTRL = ADC_REFSEL_INT1V_gc | ADC_BANDGAP_bm;
+  Adc->REFCTRL = ADC_REFSEL_VCC_gc;
+  Adc->CH0.MUXCTRL = ADC_CH_MUXINT_SCALEDVCC_gc; 
+  /* start conversion in single ended */
+  Adc->CH0.CTRL = ADC_CH_START_bm | ADC_CH_INPUTMODE_INTERNAL_gc; 
+  
+  /* wait for end of conversion */
+  while((Adc->INTFLAGS & ADC_CH0IF_bm) == 0x00u); 
+
+  // adc incorporates an offset of 205 to ensure proper 0V measure)
+  return (Adc->CH0RES - 225u);
 }
--- projects/color_proximity_sensor/adc.h
+++ projects/color_proximity_sensor/adc.h
@@ -5,5 +5,6 @@
 #include <avr/io.h>
 
 uint16_t adc_GetValue(ADC_t *Adc, uint8_t channel);
+uint16_t adc_Get3v3Value(ADC_t *Adc);
 
 #endif //ADC_H
