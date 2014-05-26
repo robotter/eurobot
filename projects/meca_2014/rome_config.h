f995225bf6c93de56c7f95586fbab26859586f76
Jean Damien Brossillon <jeandamien.brossillon@gmail.com>
2014-05-26 15:57:09 +0200

    meca: add forgotten files

---
+++ projects/meca_2014/rome_config.h
@@ -0,0 +1,24 @@
+/** @addtogroup rome */
+//@{
+/** @file
+ * @brief ROME configuration
+ */
+/** @name Configuration
+ */
+//@{
+
+
+/** @brief Disabled interrupt level when sending ROME frames
+ *
+ * If ROME frames are sent from the main and and an interrupt routine, frames
+ * will be mixed up.
+ * If this value is set, ROME frames can be safely sent from any interrupt of
+ * configured (or lower) level.
+ *
+ * @sa ROME_SEND_INTLVL_DISABLE()
+ */
+#define ROME_SEND_INTLVL  INTLVL_LO
+
+
+//@}
+//@}
---
+++ projects/meca_2014/telemetry.h
@@ -0,0 +1,43 @@
+/*  
+ *  Copyright RobOtter (2014)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TELEMETRY_H
+#define TELEMETRY_H
+
+#include <rome/rome.h>
+#include "config.h"
+
+extern rome_intf_t rome;
+
+#define TM_FREQUENCY (3)
+#define TM_PRESCALER ((int32_t)1e6/((int32_t)TM_FREQUENCY*UPDATE_ARM_US))
+
+#define _DWZ(f) do{f}while(0)
+
+#define _PRESCALE(N,f) _DWZ(\
+static int32_t _prescaler = 0;\
+if(_prescaler++>(N)) {\
+  _prescaler = 0;\
+  f\
+})
+
+#define TM_PERIODIC(f) _PRESCALE(TM_PRESCALER, f;)
+#define TM_DL_ARM(a,b,c) _PRESCALE(TM_PRESCALER, ROME_SEND_MECA_TM_ARM(&rome,a,b,c))
+#define TM_DL_SUCKERS(a,b) _PRESCALE(TM_PRESCALER, ROME_SEND_MECA_TM_SUCKERS(&rome,a,b))
+
+#endif//TELEMETRY_H
