1d1e846fab6544a63c2f81aea24d2819d0b969fc
Benoît Ryder <benoit@ryder.fr>
2014-05-27 13:47:43 +0200

    sabotter: don't initialize asserv, temporarily disable pumps
    Asserv values are not the right ones (yet).
    We don't need pumps and suckers for tests.

--- projects/sabotter_2014/strat.c
+++ projects/sabotter_2014/strat.c
@@ -70,26 +70,23 @@ void arm_set(uint16_t shoulder, uint16_t elbow, uint16_t wrist)
 
 void strat_init_galipeur(void)
 {
-  // wait for other boards to be ready
-  _delay_ms(2000);
-
   // initialize asserv variables
   //ROME_SEND_AND_WAIT(ASSERV_SET_X_PID, &rome_asserv, 3000, 0, 0, 50000, 100000, 0);
   //ROME_SEND_AND_WAIT(ASSERV_SET_Y_PID, &rome_asserv, 3000, 0, 0, 50000, 100000, 0);
   //ROME_SEND_AND_WAIT(ASSERV_SET_A_PID, &rome_asserv, 2500, 0, 0, 50000, 1000, 0);
-  ROME_SEND_AND_WAIT(ASSERV_SET_A_QRAMP, &rome_asserv, 200.0, 100.0);
-  ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_CRUISE, &rome_asserv, 20.0, 100.0);
-  ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_STEERING, &rome_asserv, 5.0, 0.1);
-  ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_STOP, &rome_asserv, 1.0, 0.1);
-  ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XYSTEERING_WINDOW, &rome_asserv, 50.0);
-  ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_STOP_WINDOWS, &rome_asserv, 5.0, 0.03);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_A_QRAMP, &rome_asserv, 200.0, 100.0);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_CRUISE, &rome_asserv, 20.0, 100.0);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_STEERING, &rome_asserv, 5.0, 0.1);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XY_STOP, &rome_asserv, 1.0, 0.1);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_XYSTEERING_WINDOW, &rome_asserv, 50.0);
+  //ROME_SEND_AND_WAIT(ASSERV_SET_HTRAJ_STOP_WINDOWS, &rome_asserv, 5.0, 0.03);
 
   // initialize meca
   ROME_SEND_AND_WAIT(MECA_SET_ARM, &rome_meca, 0, 0, 0);
-  ROME_SEND_AND_WAIT(MECA_SET_PUMP, &rome_meca, 0, 1);
-  ROME_SEND_AND_WAIT(MECA_SET_PUMP, &rome_meca, 1, 1);
-  ROME_SEND_AND_WAIT(MECA_SET_SUCKER, &rome_meca, 0, 1);
-  ROME_SEND_AND_WAIT(MECA_SET_SUCKER, &rome_meca, 1, 1);
+  //ROME_SEND_AND_WAIT(MECA_SET_PUMP, &rome_meca, 0, 1);
+  //ROME_SEND_AND_WAIT(MECA_SET_PUMP, &rome_meca, 1, 1);
+  //ROME_SEND_AND_WAIT(MECA_SET_SUCKER, &rome_meca, 0, 1);
+  //ROME_SEND_AND_WAIT(MECA_SET_SUCKER, &rome_meca, 1, 1);
   //_delay_ms(1000);
   //ROME_SEND_AND_WAIT(MECA_SET_POWER, &rome_meca, 0);
 }
