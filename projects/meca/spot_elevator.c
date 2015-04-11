8ef680ac8d91d2d57cfccf4d7a8c48c19cf8e853
lamygalle@gmail.com <lamygalle@gmail.com>
2015-04-11 12:31:33 +0200

    color_defs.h
    missing files added for meca and spot handling

---
+++ projects/meca/spot_elevator.c
@@ -0,0 +1,496 @@
+#include "spot_elevator.h"
+#include <ax12/ax12.h>
+#include <pwm/motor.h>
+#include "color_defs.h"
+#include <avarix.h>
+#include <stdlib.h>
+
+#include <rome/rome.h>
+
+extern ax12_t ax12;
+
+extern robot_color_t robot_color;
+
+#define AX12_ELEVATOR_POS_ERROR 10
+
+// local functions
+extern rome_intf_t rome;
+
+
+// set the claw ax12 to one of the _claw_ax12_positions_t enum positions
+void _set_claw_ax12(spot_elevator_t *elevator, _claw_ax12_positions_t pos)
+{
+  ax12_write_word(&ax12,
+                  elevator->claw_ax12_addr, 
+                  AX12_ADDR_GOAL_POSITION_L, 
+                  elevator->claw_ax12_positions[pos]);
+}
+
+
+void _set_elevator_ax12(spot_elevator_t *elevator, _elevator_ax12_positions_t pos)
+{
+  ax12_write_word(&ax12,
+                  elevator->elevator_ax12_addr,
+                  AX12_ADDR_GOAL_POSITION_L, 
+                  elevator->elevator_ax12_positions[pos]);
+}
+
+bool _is_claw_ax12_in_position(spot_elevator_t *elevator, _elevator_ax12_positions_t pos)
+{
+  uint16_t read_pos = 0;
+
+  ax12_read_word(&ax12,
+                  elevator->claw_ax12_addr,
+                  AX12_ADDR_PRESENT_POSITION_L,
+                  &read_pos);
+  int16_t diff = (int16_t)elevator->claw_ax12_positions[pos] - (int16_t) read_pos;
+
+  if ( ((diff >=0) && (diff < AX12_ELEVATOR_POS_ERROR)) ||  ((diff < 0) && (diff >- AX12_ELEVATOR_POS_ERROR)) )
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool _is_elevator_ax12_in_position(spot_elevator_t *elevator, _elevator_ax12_positions_t pos)
+{
+  uint16_t read_pos = 0;
+
+  ax12_read_word(&ax12,
+                  elevator->elevator_ax12_addr,
+                  AX12_ADDR_PRESENT_POSITION_L,
+                  &read_pos);
+  int16_t diff = (int16_t)elevator->elevator_ax12_positions[pos] - (int16_t) read_pos;
+
+  if ( ((diff >=0) && (diff < AX12_ELEVATOR_POS_ERROR)) ||  ((diff < 0) && (diff >- AX12_ELEVATOR_POS_ERROR)) )
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// => state machine functions
+_spot_elevator_state_t _sesm_init(spot_elevator_t *elevator)
+{
+  ax12_write_word(&ax12,
+                  elevator->claw_ax12_addr,
+                  AX12_ADDR_TORQUE_LIMIT_L,
+                  0x0060);
+ 
+  ax12_write_word(&ax12,
+                  elevator->claw_ax12_addr,
+                  AX12_ADDR_TORQUE_LIMIT_L,
+                  0x0060);
+
+  ax12_write_word(&ax12,
+                  elevator->elevator_ax12_addr,
+                  AX12_ADDR_MOVING_SPEED_L,
+                  0x0040);
+
+  return SESM_INACTIVE; 
+}
+
+_spot_elevator_state_t _sesm_inactive(spot_elevator_t *elevator)
+{
+  return SESM_INACTIVE; 
+}
+
+_spot_elevator_state_t _sesm_prepare_claw_for_onboard_buld(spot_elevator_t *elevator)
+{
+  _set_claw_ax12(elevator, CLAW_CLOSED_FOR_BULB);
+  _set_elevator_ax12(elevator, ELEVATOR_UP);
+
+  return SESM_INACTIVE; 
+}
+
+_spot_elevator_state_t _sesm_open_claw_for_bulb(spot_elevator_t *elevator)
+{
+  _set_claw_ax12(elevator, CLAW_OPENED);
+  _set_elevator_ax12(elevator, ELEVATOR_DOWN_WAITING_SPOT);
+  return SESM_INACTIVE; 
+}
+
+_spot_elevator_state_t _sesm_check_spot_presence(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+
+  if (elevator->is_spot_present != NULL)
+  {
+    if (true == elevator->is_spot_present())
+    {
+     // spot detected => check color 
+      next_state = SESM_CHECK_SPOT_COLOR;
+    }
+    else
+    {
+      // continue waiting for spot
+      next_state = SESM_CHECK_SPOT_PRESENCE;
+    }
+  }
+  return next_state;
+}
+
+_spot_elevator_state_t _sesm_check_spot_color(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  if (elevator->get_spot_color != NULL)
+  {
+    if (robot_color == elevator->get_spot_color())
+    {
+      // good spot color => go to next state
+      next_state = SESM_CHECK_CLAW_OPENED;
+    }
+    else
+    {
+      // wrong color => go to spot detection state
+      next_state = SESM_CHECK_SPOT_PRESENCE;
+    }
+  }
+  return next_state;
+}
+
+_spot_elevator_state_t _sesm_check_elevator_position(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  // check if claw is up to allow
+  if (true == _is_elevator_ax12_in_position(elevator, ELEVATOR_UP))
+  {
+    /// TODO
+    (void)next_state;
+  }
+  
+  return next_state; 
+}
+
+_spot_elevator_state_t _sesm_check_claw_opened(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  
+  if ( true == _is_claw_ax12_in_position(elevator, CLAW_OPENED))
+  {
+    // claw opened => go to elevator lift down
+    next_state = SESM_LIFT_DOWN_ELEVATOR;
+  }
+  else
+  {
+    next_state = SESM_OPEN_CLAW;
+  }
+  return next_state; 
+}
+
+_spot_elevator_state_t _sesm_open_claw(spot_elevator_t *elevator)
+{
+  _set_claw_ax12(elevator, CLAW_OPENED);
+  return SESM_WAIT_CLAW_OPENED; 
+}
+
+_spot_elevator_state_t _sesm_wait_claw_opened(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  
+  if (true == _is_claw_ax12_in_position(elevator, CLAW_OPENED))
+  {
+    next_state = SESM_LIFT_DOWN_ELEVATOR;
+  }
+  else
+  {
+    next_state = SESM_WAIT_CLAW_OPENED;
+  }
+
+  return next_state; 
+}
+
+_spot_elevator_state_t _sesm_lift_down_elevator(spot_elevator_t *elevator)
+{
+  _set_elevator_ax12(elevator, ELEVATOR_DOWN_WAITING_SPOT);
+  return SESM_WAIT_ELEVATOR_DOWN;
+}
+
+_spot_elevator_state_t _sesm_wait_elevator_down(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+
+  if (true == _is_elevator_ax12_in_position(elevator, ELEVATOR_DOWN_WAITING_SPOT))
+  {
+    // elevator down => go to next state
+    next_state = SESM_CLOSE_CLAW;
+  }
+  else
+  {
+    next_state = SESM_WAIT_ELEVATOR_DOWN;
+  }
+  return next_state; 
+}
+
+_spot_elevator_state_t _sesm_close_claw(spot_elevator_t *elevator)
+{
+  _set_claw_ax12(elevator, CLAW_CLOSED_FOR_SPOT);
+  return SESM_WAIT_CLAW_CLOSED;
+}
+
+_spot_elevator_state_t _sesm_wait_claw_closed(spot_elevator_t *elevator)
+{
+  /// TODO check if the correct way to check it is not simply torque
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+
+  if (true == _is_claw_ax12_in_position(elevator, CLAW_CLOSED_FOR_SPOT))
+  {
+    // claw closed => go to next state
+    next_state = SESM_LIFT_UP_ELEVATOR;
+  }
+  else
+  {
+    // claw not closed yet
+    next_state = SESM_WAIT_CLAW_CLOSED;
+  }
+
+  return next_state;
+}
+
+_spot_elevator_state_t _sesm_lift_up_elevator(spot_elevator_t *elevator)
+{
+  _set_elevator_ax12(elevator, ELEVATOR_UP);
+  return SESM_WAIT_ELEVATOR_UP;
+}
+
+_spot_elevator_state_t _sesm_wait_elevator_up(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  
+  if( true == _is_elevator_ax12_in_position(elevator, ELEVATOR_UP))
+  {
+    //next_state = SESM_CHECK_SPOT_PRESENCE;
+    next_state = SESM_INACTIVE;
+  }
+  else
+  {
+    // continue in this step
+    next_state = SESM_WAIT_ELEVATOR_UP;
+  }
+
+
+  return next_state;
+}
+
+_spot_elevator_state_t _sesm_close_claw_for_bulb(spot_elevator_t *elevator)
+{
+  _set_claw_ax12(elevator, CLAW_CLOSED_FOR_BULB);
+  return SESM_WAIT_CLAW_CLOSED_FOR_BULB; 
+}
+
+_spot_elevator_state_t _sesm_wait_claw_closed_for_bulb(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+  
+  if (true == _is_claw_ax12_in_position(elevator, CLAW_CLOSED_FOR_BULB))
+  {
+    next_state = SESM_LIFT_UP_ELEVATOR_FOR_BULB;
+  }
+  else
+  {
+    next_state = SESM_WAIT_CLAW_CLOSED_FOR_BULB;
+  }
+  return next_state;
+}
+
+_spot_elevator_state_t _sesm_lift_up_elevator_for_bulb(spot_elevator_t *elevator)
+{
+  _set_elevator_ax12(elevator, ELEVATOR_UP);
+  return SESM_WAIT_ELEVATOR_UP_FOR_BULB;
+}
+
+_spot_elevator_state_t _sesm_wait_elevator_up_for_bulb(spot_elevator_t *elevator)
+{
+  _spot_elevator_state_t next_state = SESM_INACTIVE;
+
+  if (true == _is_elevator_ax12_in_position(elevator, ELEVATOR_UP))
+  {
+    // elevator up => ready to check color
+    next_state = SESM_CHECK_SPOT_PRESENCE;
+  }
+  else
+  {
+    // elevator not ready => stay in this state
+    next_state = SESM_WAIT_ELEVATOR_UP_FOR_BULB;
+  }
+  return next_state;
+}
+
+// public functions
+void spot_elevator_init(spot_elevator_t *elevator)
+{
+  if (elevator!= NULL)
+  {
+    elevator->sm_state = SESM_INIT;
+    elevator->is_spot_present = NULL;
+    elevator->get_spot_color = NULL;
+    elevator->is_active = false;
+  }
+}
+void spot_elevator_set_claw_ax12_addr(spot_elevator_t *elevator, ax12_addr_t addr)
+{
+  if (elevator!= NULL)
+  {
+    elevator->claw_ax12_addr = addr; 
+  }
+}
+
+void spot_elevator_set_elevator_ax12_addr(spot_elevator_t *elevator, ax12_addr_t addr)
+{
+  if (elevator!= NULL)
+  {
+    elevator->elevator_ax12_addr = addr;
+  }
+}
+
+void spot_elevator_set_is_spot_present_fn(spot_elevator_t *elevator, bool (*is_spot_present)(void))
+{
+  if (elevator!= NULL)
+  {
+    elevator->is_spot_present = is_spot_present;
+  }
+}
+
+void spot_elevator_set_get_spot_color_fn(spot_elevator_t *elevator, robot_color_t(*get_spot_color)(void))
+{
+  if (elevator!= NULL)
+  {
+    elevator->get_spot_color = get_spot_color;
+  }
+}
+
+void spot_elevator_manage(spot_elevator_t *elevator)
+{
+  if ((elevator != NULL) && 
+      (true == elevator->is_active))
+  {
+  ROME_LOGF(&rome, DEBUG, "state %u", (uint16_t)elevator->sm_state);
+
+    // each state uses a non blocking function that returns the next state of the state machine
+    switch(elevator->sm_state)
+    {
+     case SESM_INIT:
+       elevator->sm_state = _sesm_init(elevator);
+       break;
+
+     case SESM_INACTIVE:
+       elevator->sm_state = _sesm_inactive(elevator);
+       break;
+
+     case SESM_PREPARE_CLAW_FOR_ONBOARD_BULB:
+       elevator->sm_state = _sesm_prepare_claw_for_onboard_buld(elevator);
+       break;
+
+     case SESM_OPEN_CLAW_FOR_BULB:
+       elevator->sm_state = _sesm_open_claw_for_bulb(elevator);
+       break;
+
+     case SESM_CHECK_SPOT_PRESENCE:
+       elevator->sm_state = _sesm_check_spot_presence(elevator);
+       break;
+
+     case  SESM_CHECK_SPOT_COLOR:
+       elevator->sm_state = _sesm_check_spot_color(elevator);
+       break;
+
+     case SESM_CHECK_ELEVATOR_POSITION:
+       elevator->sm_state = _sesm_check_elevator_position(elevator);
+       break;
+
+     case  SESM_CHECK_CLAW_OPENED:
+       elevator->sm_state = _sesm_check_claw_opened(elevator);
+       break;
+
+     case SESM_OPEN_CLAW: 
+       elevator->sm_state = _sesm_open_claw(elevator);
+       break;
+
+     case SESM_WAIT_CLAW_OPENED:
+       elevator->sm_state = _sesm_wait_claw_opened(elevator);
+       break;
+
+     case SESM_LIFT_DOWN_ELEVATOR:
+       elevator->sm_state = _sesm_lift_down_elevator(elevator);
+       break;
+
+     case SESM_WAIT_ELEVATOR_DOWN:
+       elevator->sm_state = _sesm_wait_elevator_down(elevator);
+       break;
+
+     case SESM_CLOSE_CLAW:
+       elevator->sm_state = _sesm_close_claw(elevator);
+       break;
+
+     case SESM_WAIT_CLAW_CLOSED:
+       elevator->sm_state = _sesm_wait_claw_closed(elevator);
+       break;
+
+     case SESM_LIFT_UP_ELEVATOR:
+       elevator->sm_state = _sesm_lift_up_elevator(elevator);
+       break;
+
+     case SESM_WAIT_ELEVATOR_UP:
+       elevator->sm_state = _sesm_wait_elevator_up(elevator);
+       break;
+
+     case SESM_CLOSE_CLAW_FOR_BULB:
+       elevator->sm_state = _sesm_close_claw_for_bulb(elevator);
+       break;
+
+     case SESM_WAIT_CLAW_CLOSED_FOR_BULB:
+       elevator->sm_state = _sesm_wait_claw_closed_for_bulb(elevator);
+       break;
+
+     case SESM_LIFT_UP_ELEVATOR_FOR_BULB:
+       elevator->sm_state = _sesm_lift_up_elevator_for_bulb(elevator);
+       break;
+
+     case SESM_WAIT_ELEVATOR_UP_FOR_BULB:
+       elevator->sm_state = _sesm_wait_elevator_up_for_bulb(elevator);
+       break;
+
+      default : 
+      // shall not arrive !!!
+       elevator->sm_state = SESM_INACTIVE;
+       break;
+    }
+  }
+}
+
+
+void spot_elevator_set_enable(spot_elevator_t *se, bool enable)
+{
+  se->is_active = enable;
+}
+
+void spot_elevator_prepare_for_bulb_picking(spot_elevator_t *se)
+{
+  se->sm_state = SESM_OPEN_CLAW_FOR_BULB;
+}
+
+void spot_elevator_prepare_for_onboard_bulb(spot_elevator_t *se)
+{
+  se->sm_state = SESM_PREPARE_CLAW_FOR_ONBOARD_BULB;
+}
+
+void spot_elevator_pick_bulb(spot_elevator_t *se)
+{
+  se->sm_state = SESM_CLOSE_CLAW_FOR_BULB;
+}
+
+void spot_elevator_automatic_spot_stacking(spot_elevator_t *se)
+{
+  se->sm_state = SESM_CHECK_SPOT_PRESENCE;
+}
+
+void spot_elevator_discharge_spot_stack(spot_elevator_t *se)
+{
+  /// TODO
+}
+
---
+++ projects/meca/spot_elevator.h
@@ -0,0 +1,120 @@
+#ifndef SPOT_ELEVATOR_H
+#define SPOT_ELEVATOR_H
+
+#include <ax12/ax12.h>
+#include <pwm/motor.h>
+#include "color_defs.h"
+/* 
+ * In this module, spipe defines the "spot pipe" used to contain the spots
+ */
+
+typedef enum{
+  CLAW_OPENED = 0,
+  CLAW_CLOSED_FOR_SPOT,
+  CLAW_CLOSED_FOR_BULB,
+
+  // CLAW_AX12_POSITIONS_LENGTH must be the last element of this enum
+  CLAW_AX12_POSITIONS_LENGTH
+} _claw_ax12_positions_t;
+
+typedef enum{
+  ELEVATOR_UP = 0,
+  ELEVATOR_DOWN_WAITING_SPOT,
+  ELEVATOR_DOWN_WAITING_BULB,
+
+  // ELEVATOR_AX12_POSITIONS_LENGTH must be the last element of this enum
+  ELEVATOR_AX12_POSITIONS_LENGTH
+} _elevator_ax12_positions_t;
+
+typedef enum{
+  SPIPE_CLOSED = 0,
+  SPIPE_OPENED,
+
+  // ELEVATOR_AX12_POSITIONS_LENGTH must be the last element of this enum
+  SPIPE_POSITIONS_LENGTH
+} _spipe_positions_t;
+
+// states of the state machine that handles the elevator and claw
+typedef enum{
+  SESM_INIT,
+  SESM_INACTIVE,
+  // prepare claw to retain bulb boarded during preparation of match
+  SESM_PREPARE_CLAW_FOR_ONBOARD_BULB,
+  // second init loop : bulb picking (tennis ball)
+  SESM_OPEN_CLAW_FOR_BULB,
+  
+  // first main loop : spot handling
+  SESM_CHECK_SPOT_PRESENCE,
+  SESM_CHECK_SPOT_COLOR,
+  SESM_CHECK_ELEVATOR_POSITION,
+  SESM_CHECK_CLAW_OPENED,
+  SESM_OPEN_CLAW,
+  SESM_WAIT_CLAW_OPENED,
+  SESM_LIFT_DOWN_ELEVATOR,
+  SESM_WAIT_ELEVATOR_DOWN,
+  SESM_CLOSE_CLAW,
+  SESM_WAIT_CLAW_CLOSED,
+  SESM_LIFT_UP_ELEVATOR,
+  SESM_WAIT_ELEVATOR_UP,
+  // second main loop : prepare claw for bulb picking
+  SESM_CLOSE_CLAW_FOR_BULB,
+  SESM_WAIT_CLAW_CLOSED_FOR_BULB,
+  SESM_LIFT_UP_ELEVATOR_FOR_BULB,
+  SESM_WAIT_ELEVATOR_UP_FOR_BULB
+
+}_spot_elevator_state_t;
+
+typedef struct{
+  ax12_addr_t claw_ax12_addr;
+  ax12_addr_t elevator_ax12_addr;
+  // pwm used to control analog servo that controls the spot pipe opening.
+  pwm_motor_t spipe_pwm; 
+  
+  // table of positions for ax12 used to control claw. Use _claw_ax12_positions_t enum to access to table.
+  int16_t claw_ax12_positions[CLAW_AX12_POSITIONS_LENGTH];
+
+  // table of positions for ax12 used to control elevator. Use _elevator_ax12_positions_t enum to access to table.
+  int16_t elevator_ax12_positions[ELEVATOR_AX12_POSITIONS_LENGTH];
+  
+  // table of positions for analog servo that controls the spot pipe (spipe). Use _spipe_positions_t enum to access to table.
+  int16_t spipe_pwm_positions[ELEVATOR_AX12_POSITIONS_LENGTH];
+
+  // accessors that return spot presence and color
+  bool (*is_spot_present)(void);
+  robot_color_t (*get_spot_color)(void);
+ 
+  // state of the state machine handles in spot_elevator_manage
+  _spot_elevator_state_t sm_state;
+
+  // indicates if the elevator is active or not
+  bool is_active;
+
+}spot_elevator_t;
+
+
+// initialize spot_elevator_t structure with default data value
+void spot_elevator_init(spot_elevator_t *elevator);
+
+// accessors to set addresses of ax12 used to control elevators.
+void spot_elevator_set_claw_ax12_addr(spot_elevator_t *elevator, ax12_addr_t addr);
+void spot_elevator_set_elevator_ax12_addr(spot_elevator_t *elevator, ax12_addr_t addr);
+
+void spot_elevator_set_is_spot_present_fn(spot_elevator_t *elevator, bool (*is_spot_present)(void));
+void spot_elevator_set_get_spot_color_fn(spot_elevator_t *elevator, robot_color_t(*get_spot_color)(void));
+
+// function that must be called periodically to manage the spot elevator
+void spot_elevator_manage(spot_elevator_t *elevator);
+
+void spot_elevator_set_enable(spot_elevator_t *se, bool enable);
+
+void spot_elevator_prepare_for_bulb_picking(spot_elevator_t *se);
+
+void spot_elevator_prepare_for_onboard_bulb(spot_elevator_t *se);
+
+void spot_elevator_pick_bulb(spot_elevator_t *se);
+
+void spot_elevator_automatic_spot_stacking(spot_elevator_t *se);
+
+void spot_elevator_discharge_spot_stack(spot_elevator_t *se);
+
+#endif //SPOT_ELEVATOR_H
