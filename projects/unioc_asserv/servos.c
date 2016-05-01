8f37e3d7f9d410c3e44177bdd69c8af0871eaece
og <augustin.boisvert@gmail.com>
2016-05-01 17:06:03 +0200

    unioc : servos files added

---
+++ projects/unioc_asserv/servos.c
@@ -0,0 +1,45 @@
+/*  
+ *  Copyright RobOtter (2016)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <avarix.h>
+#include <pwm/motor.h>
+#include "servos.h"
+pwm_motor_t servos[2];
+
+void servos_init (void){
+  pwm_servo_init(servos,   (TC0_t *) &TCF1, 'A');
+  pwm_servo_init(servos+1, (TC0_t *) &TCF1, 'B');
+
+  pwm_motor_set_frequency(servos,   50u);
+  pwm_motor_set_frequency(servos+1, 50u);
+}
+
+void servo_set(uint8_t id, uint16_t value){
+  uint16_t pwm = (value < PWM_MOTOR_MAX) ? value : PWM_MOTOR_MAX;
+  switch(id){
+    case 0:
+      pwm_motor_set(servos, pwm);
+      break;
+    case 1:
+      pwm_motor_set(servos+1, pwm);
+      break;
+    default:
+      break;
+   }
+}
+  
