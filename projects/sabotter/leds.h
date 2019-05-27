8736e7c1ed531cc79cfad9f26b72c6a7f3fa3618
Benoît Ryder <benoit@ryder.fr>
2019-05-27 16:22:32 +0200

    strat: fix RGB led colors

--- projects/sabotter/leds.h
+++ projects/sabotter/leds.h
@@ -11,12 +11,12 @@
 } while(0)
 
 typedef enum {
-  LED_COLOR_RED      = 0b001,
+  LED_COLOR_RED      = 0b100,
   LED_COLOR_GREEN    = 0b010,
-  LED_COLOR_BLUE     = 0b100,
-  LED_COLOR_YELLOW   = 0b011,
+  LED_COLOR_BLUE     = 0b001,
+  LED_COLOR_YELLOW   = 0b110,
   LED_COLOR_MAGENTA  = 0b101,
-  LED_COLOR_CYAN     = 0b110,
+  LED_COLOR_CYAN     = 0b011,
   LED_COLOR_WHITE    = 0b111,
   LED_COLOR_BLACK    = 0b000,
 } ledrgb_color_t;
